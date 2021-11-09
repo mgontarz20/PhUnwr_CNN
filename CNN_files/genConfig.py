9cf4be5ec5b7d77938bc833ce5dbcf7a4b2e6971
Michał Gontarz <mgontarz20@gmail.com>
2021-11-09 19:28:18 +0000

    Changes to make read evaluation in configs more readable.

--- CNN_files/genConfig.py
+++ CNN_files/genConfig.py
@@ -1,10 +1,14 @@
-def write_cfg(dir, name, op, **kwargs):
+def write_cfg(dir, name, op,strip_newline = False, **kwargs):
 
 
     f = open(f"{dir}/{name}.txt", op)
     if op == 'w':
         f.write(f"dir:{dir}\nname: {name}\n")
-    for key,value in kwargs.items():
-        f.write(f"{key}: {value}\n")
+    if strip_newline:
+        for key, value in kwargs.items():
+            f.write(f"{key}: {value}")
+    else:
+        for key, value in kwargs.items():
+            f.write(f"{key}: {value}\n")
 
     f.close()
